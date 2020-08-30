a32f08747caf23eddb43ae7f623aa6699d93ac23
obtusedev <upc77@protonmail.com>
2020-08-29 18:28:54 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`app started on http://localhost:${port}`);
+})
